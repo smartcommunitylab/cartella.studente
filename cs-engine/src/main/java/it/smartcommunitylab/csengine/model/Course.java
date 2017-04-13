e86350ff11d7e143bb2a4489e2be0b6741614a8c
micnori <micnori@yahoo.com>
2017-04-13 17:12:51 +0200

    engine: add Course

--- cs-engine/src/main/java/it/smartcommunitylab/csengine/model/Course.java
+++ cs-engine/src/main/java/it/smartcommunitylab/csengine/model/Course.java
@@ -2,25 +2,14 @@ package it.smartcommunitylab.csengine.model;
 
 import java.util.Date;
 
-public class Registration extends BaseObject {
-	private String studentId;
+public class Course extends BaseObject {
 	private String instituteId;
-	private String courseId;
 	private String schoolYear;
 	private Date dateFrom;
 	private Date dateTo;
 	private String course;
 	private String teachingUnit;
-	private String classroom;
-	private Institute institute;
-	private Student student; 
 	
-	public String getStudentId() {
-		return studentId;
-	}
-	public void setStudentId(String studentId) {
-		this.studentId = studentId;
-	}
 	public String getInstituteId() {
 		return instituteId;
 	}
@@ -51,34 +40,12 @@ public class Registration extends BaseObject {
 	public void setCourse(String course) {
 		this.course = course;
 	}
-	public String getClassroom() {
-		return classroom;
-	}
-	public void setClassroom(String classroom) {
-		this.classroom = classroom;
-	}
-	public Institute getInstitute() {
-		return institute;
-	}
-	public void setInstitute(Institute institute) {
-		this.institute = institute;
-	}
-	public Student getStudent() {
-		return student;
-	}
-	public void setStudent(Student student) {
-		this.student = student;
-	}
 	public String getTeachingUnit() {
 		return teachingUnit;
 	}
 	public void setTeachingUnit(String teachingUnit) {
 		this.teachingUnit = teachingUnit;
 	}
-	public String getCourseId() {
-		return courseId;
-	}
-	public void setCourseId(String courseId) {
-		this.courseId = courseId;
-	}
+	
+
 }
---
+++ cs-engine/src/main/java/it/smartcommunitylab/csengine/storage/CourseRepository.java
@@ -0,0 +1,19 @@
+package it.smartcommunitylab.csengine.storage;
+
+import it.smartcommunitylab.csengine.model.Course;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface CourseRepository extends MongoRepository<Course, String> {
+	List<Course> findAll(Sort sort);
+	
+	@Query(value="{instituteId:?0, schoolYear:?1}")
+	List<Course> findByInstitute(String instituteId, String schoolYear);
+	
+	@Query(value="{origin:?0, extId:?1}")
+	Course findByExtId(String origin, String extId);
+}
