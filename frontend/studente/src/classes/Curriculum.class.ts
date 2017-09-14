5e47b090d705f03e24c9f93428e050d595c87f8d
Nawaz Khurshid <nawaz1981@gmail.com>
2017-09-14 17:12:11 +0200

    Added FileSaver plugin

--- frontend/studente/src/classes/Curriculum.class.ts
+++ frontend/studente/src/classes/Curriculum.class.ts
@@ -6,6 +6,18 @@ import { Student } from './Student.class';
 export class Curriculum {
 
     id: string;
+    attachments: String[];
+    creationDate: string;
+    cvLangCertList: Certification[];
+    cvMobilityList: Mobility[];
+    cvStageList: Stage[];
+    extId: string;
+    lastUpdate: string;
+    origin: string;
+    registrationIdList: string[];
+    storageIdList: string[];
+    student: Student[];
+    studentExperienceIdMap: {};
     studentId: string
 
 }
