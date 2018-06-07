64678c64d058148e3591c317798ec2c11782888c
gioadami <giordano.adami@gmail.com>
2018-06-07 15:13:49 +0200

    Added/updated API.

--- cs-engine/src/main/java/it/smartcommunitylab/csengine/controller/StatisticsController.java
+++ cs-engine/src/main/java/it/smartcommunitylab/csengine/controller/StatisticsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import it.smartcommunitylab.csengine.model.Course;
+import it.smartcommunitylab.csengine.model.statistics.CourseData;
 import it.smartcommunitylab.csengine.model.statistics.KPI;
 import it.smartcommunitylab.csengine.model.statistics.POI;
 import it.smartcommunitylab.csengine.model.statistics.StudentProfile;
@@ -21,28 +21,34 @@ public class StatisticsController {
 	private RepositoryManager dataManager;
 	
 	@GetMapping("/api/statistics/teachingUnits")
-	public List<POI> getInstitute(@RequestParam(required=false) String ordine, @RequestParam(required=false) String tipologia, @RequestParam(required=false) Double[] coordinates, @RequestParam(required=false) Double radius) throws Exception {
-		List<POI> result = dataManager.findTeachingUnit(ordine, tipologia, coordinates, radius);
+	public List<POI> getTeachingUnits(@RequestParam(required=false) String ordine, @RequestParam(required=false) String tipologia, @RequestParam(required=false) Double[] coordinates, @RequestParam(required=false) Double radius, @RequestParam(required=false) String schoolYear) throws Exception {
+		List<POI> result = dataManager.findTeachingUnits(ordine, tipologia, coordinates, radius, schoolYear);
 		return result;
 	}
 	
+	@GetMapping("/api/statistics/institutes")
+	public List<POI> getInstitutes(@RequestParam(required=false) String ordine, @RequestParam(required=false) String tipologia, @RequestParam(required=false) Double[] coordinates, @RequestParam(required=false) Double radius, @RequestParam(required=false) String schoolYear) throws Exception {
+		List<POI> result = dataManager.findInstitutes(ordine, tipologia, coordinates, radius, schoolYear);
+		return result;
+	}	
+	
 	@GetMapping("/api/statistics/courses")
-	public List<Course> getInstituteCourses(@RequestParam(required=false) String teachingUnitId, @RequestParam(required=false) String schoolYear) throws Exception {
-		List<Course> result = dataManager.findCourses(teachingUnitId, schoolYear);
+	public List<CourseData> getInstituteCourses(@RequestParam(required=false) String teachingUnitId, @RequestParam(required=false) String instituteId, @RequestParam(required=false) String schoolYear) throws Exception {
+		List<CourseData> result = dataManager.findCourses(teachingUnitId, instituteId, schoolYear);
 		return result;
 	}	
 	
 	@GetMapping("/api/statistics/kpi")
-	public List<KPI> getInstituteKPIs(@RequestParam(required=false) String teachingUnitId, @RequestParam(required=false) String schoolYear) throws Exception {
-		List<KPI> result = dataManager.getInstituteKPIs(teachingUnitId, schoolYear);
+	public List<KPI> getInstituteKPIs(@RequestParam(required=true) String instituteId, @RequestParam(required=true) String schoolYear) throws Exception {
+		List<KPI> result = dataManager.getInstituteKPIs(instituteId, schoolYear);
 		return result;
 	}	
 	
-	@GetMapping("/api/statistics/student")
-	public StudentProfile getStudentProfile(@RequestParam(required=false) String studentId) throws Exception {
-		StudentProfile result = dataManager.getStudentProfile(studentId);
-		return result;
-	}		
+//	@GetMapping("/api/statistics/student")
+//	public StudentProfile getStudentProfile(@RequestParam(required=false) String studentId) throws Exception {
+//		StudentProfile result = dataManager.getStudentProfile(studentId);
+//		return result;
+//	}		
 	
 //	@GetMapping("/api/statistics/count")
 //	public void count() {
