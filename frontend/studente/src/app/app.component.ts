d6718bf96b28a6e7e526049b94753e27bbfab210
Nawaz Khurshid <nawaz1981@gmail.com>
2018-10-05 16:21:52 +0200

    Fix for default page

--- frontend/studente/src/app/app.component.ts
+++ frontend/studente/src/app/app.component.ts
@@ -4,18 +4,23 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import {TranslateService} from 'ng2-translate';
 import {LoginService, LOGIN_STATUS} from '../services/login.service';
-
 import { LoginPage } from '../pages/login/login';
-import { HomePage } from '../pages/home/home';
-import { TermsPage } from '../pages/terms/terms';
+
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage: any;
-
+ 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, translate: TranslateService, private login: LoginService) {
+
+    if (!sessionStorage.login) {
+      this.rootPage = LoginPage; 
+      sessionStorage.login = true;
+    }
+    
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -23,7 +28,7 @@ export class MyApp {
       splashScreen.hide();
       translate.setDefaultLang('it');
 
-      // this.rootPage = HomePage;
+      // this.rootPage = LoginPage;
       
       // login.checkLoginStatus().then(
       //   status => {
